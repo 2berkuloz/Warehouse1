ec6dc11e40cee2208506e6f116486599adcd8ca0
Grek <grek.offical@mail.ru>
2024-01-22 11:12:25 +0300

    При наведении курсора на кнопки отображается название кнопки

--- main_window.py
+++ main_window.py
@@ -10,6 +10,7 @@ import sys
 import random
 
 class Ui_MainWindow(object):
+    """Класс интерфейса главного окна программы"""
     COLUMN_COUNT = 6
 
     ICON_PATHS = {
@@ -22,6 +23,7 @@ class Ui_MainWindow(object):
     }
 
     def setupUi(self, MainWindow):
+        """"Метод установки параметров интерфейса главного окна программы"""
         MainWindow.setObjectName("MainWindow")
         MainWindow.setFixedSize(1060, 650)
         MainWindow.setWindowTitle("Твой склад")
@@ -37,6 +39,7 @@ class Ui_MainWindow(object):
         
 
     def setupTableWidget(self, MainWindow):
+        """"Метод настройки виджет таблицы"""
         self.tableWidget = QtWidgets.QTableWidget(parent=MainWindow)
         self.tableWidget.setGeometry(QtCore.QRect(30, 100, 981, 451))
         self.tableWidget.setAlternatingRowColors(True)
@@ -49,6 +52,7 @@ class Ui_MainWindow(object):
         self.tableWidget.setColumnWidth(0, 150)
 
     def setupButtons(self, MainWindow):
+        """Метод настройки кнопок в главном окне"""
         self.addButton = self.createButton(MainWindow, 'add', self.openAddWindow, 35, 30)
         self.editButton = self.createButton(MainWindow, 'edit', self.openEditWindow, 110, 30)
         self.deleteButton = self.createButton(MainWindow, 'delete', self.deleteRow, 175, 30)
@@ -57,6 +61,7 @@ class Ui_MainWindow(object):
         self.logButton = self.createButton(MainWindow, 'log', self.openLogWindow, 370, 30)
 
     def createButton(self, parent, icon_name, slot, x, y):
+        """Метод создания кнопок с заданными параметрами"""
         button = QtWidgets.QPushButton(parent=parent)
         button.setGeometry(QtCore.QRect(x, y, 70, 70))
         button.setIcon(QtGui.QIcon(Ui_MainWindow.ICON_PATHS.get(icon_name, '')))
@@ -66,6 +71,7 @@ class Ui_MainWindow(object):
         return button
 
     def sortByColumn(self, logicalIndex):
+        """"Метод сортировки таблицы"""
         currentOrder = self.tableWidget.horizontalHeader().sortIndicatorOrder()
         if logicalIndex == 3:
             newOrder = (QtCore.Qt.SortOrder.DescendingOrder if currentOrder == QtCore.Qt.SortOrder.AscendingOrder
@@ -79,6 +85,7 @@ class Ui_MainWindow(object):
 
     
     def create_confirmation_box(self, text, title):
+        """Метод создания диалогового окна подтверждения"""
         msg_box = QtWidgets.QMessageBox()
         msg_box.setIcon(QtWidgets.QMessageBox.Icon.Question)
         icon = QtGui.QIcon("ask.png")
@@ -88,34 +95,41 @@ class Ui_MainWindow(object):
         return msg_box
     
     def on_close_event(self, event):
+        """Метод, вызываемый при закрытии главного окна. Сохраняет данные в базу данных перед закрытием"""
         self.save_to_database()
         event.accept()
     
     def setupDatabase(self):
+        """Метод для инициализации базы данных и создания таблиц"""
         self.db = Database('products.db')
         self.db.create_table()
         self.db.create_log_table()
 
     def load_from_database(self):
+        """Метод загрузки данных из базы данных и их отображения в таблице"""
         self.clear_table()
         data = self.db.load_from_database()
         for row_data in data:
             self.insert_row_from_data(row_data)
 
     def clear_table_visibility(self):
+        """Метод сброса видимости всех строк в таблице"""
         for row in range(self.tableWidget.rowCount()):
             self.tableWidget.setRowHidden(row, False)
 
     def generate_random_id(self):
+        """Метод для генерации случайного идентификатора ID"""
         return ''.join(random.choice('0123456789') for _ in range(8))
 
     def id_exists(self, product_id):
+        """Метод проверки существования идентификатора ID в таблице"""
         for row in range(self.tableWidget.rowCount()):
             if self.tableWidget.item(row, 2).text() == product_id:
                 return True
         return False
 
     def performAction(self):
+        """Метод для выполнения действия над выбранной строкой таблицы"""
         selected_row = self.tableWidget.currentRow()
         if selected_row >= 0:
             quantity_item = self.tableWidget.item(selected_row, 3)
@@ -142,9 +156,11 @@ class Ui_MainWindow(object):
             error_box.exec()
 
     def clear_table(self):
+        """Метод очистки таблицы"""
         self.tableWidget.setRowCount(0)
 
     def show_message_box(self, message):
+        """Метод отображения информационного диалогового окна"""
         msg_box = QtWidgets.QMessageBox()
         msg_box.setIcon(QtWidgets.QMessageBox.Icon.Information)
         icon = QtGui.QIcon("good.png")
@@ -154,6 +170,7 @@ class Ui_MainWindow(object):
         msg_box.exec()
 
     def deleteRow(self):
+        """"Метод для удаления выбранной строки из таблицы"""
         selected_row = self.tableWidget.currentRow()
         if selected_row >= 0:
             msg_box = QtWidgets.QMessageBox()
@@ -190,9 +207,8 @@ class Ui_MainWindow(object):
             error_box.setWindowIcon(error_icon)
             error_box.exec()
 
-                    
-            
     def insert_row_from_data(self, row_data):
+        """Метод для вставки строки данных в таблицу"""
         row_position = self.tableWidget.rowCount()
         self.tableWidget.insertRow(row_position)
         for column, value in enumerate(row_data):
@@ -201,6 +217,7 @@ class Ui_MainWindow(object):
 
 
     def save_to_database(self):
+        """Метод сохранения данных из таблицы в базу данных"""
         self.db.clear_database()
         data = []
         for row in range(self.tableWidget.rowCount()):
@@ -217,6 +234,7 @@ class Ui_MainWindow(object):
         self.db.save_to_database(data)
 
     def openEditWindow(self):
+        """Метод открытия окна редактирования выбранной строки"""
         selected_row = self.tableWidget.currentRow()
         if selected_row >= 0:
             category_item = self.tableWidget.item(selected_row, 0)
@@ -250,6 +268,7 @@ class Ui_MainWindow(object):
             error_box.exec()
 
     def editProductInTable(self, row, category, name, product_id, quantity, size, dimensions):
+        """Метод для редактирования данных в таблице"""
         self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(category))
         self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(name))
         self.tableWidget.setItem(row, 2, QtWidgets.QTableWidgetItem(product_id))
@@ -260,9 +279,11 @@ class Ui_MainWindow(object):
         self.add_to_log("Редактирование товара", f" Категория: {category}, Название: {name}, ID: {product_id}, Количество: {quantity}, Размер: {size} Габариты: {dimensions}")
 
     def set_log_window(self, log_window):
+        """Метод установки окна журнала"""
         self.log_window = log_window
 
     def openLogWindow(self):
+        """Метод открытия окна журнала событий"""
         log_entries = self.db.load_log_from_database()
 
         if self.log_window is None or not self.log_window.isVisible():
@@ -270,11 +291,13 @@ class Ui_MainWindow(object):
             self.log_window.show() 
 
     def add_to_log(self, action, details):
+        """Метод добавления записи в журнал событий"""
         log_entry = f"{action}: {details}"
         self.log_window.database.add_log_entry_to_database(log_entry)
         self.log_window.load_log_from_database()
 
     def openAddWindow(self):
+        """Метод открытия окна добавления нового товара"""
         product_id = self.generate_random_id()
         categories = self.db.get_categories()
         self.add_window = AddWindow(product_id, self.db, categories)
@@ -282,12 +305,14 @@ class Ui_MainWindow(object):
         self.add_window.show()
 
     def openSearchWindow(self):
+        """Метод открытия окна поиска"""
         self.search_window = SearchWindow()
         self.search_window.show()
         self.search_window.searchProductSignal.connect(self.searchProductInTable)
         self.search_window.cancelSearchSignal.connect(self.clear_table_visibility)
 
     def searchProductInTable(self, search_term):
+        """Метод поиска товаров в таблице"""
         self.clear_table_visibility()
         for row in range(self.tableWidget.rowCount()):
             item_category = self.tableWidget.item(row, 0)
@@ -305,6 +330,7 @@ class Ui_MainWindow(object):
                     self.tableWidget.hideRow(row)
 
     def addProductToTable(self, category, name, product_id, quantity):
+        """Метод для добавления товара в таблицу"""
         if not self.id_exists(product_id):
             rowPosition = self.tableWidget.rowCount()
             self.tableWidget.insertRow(rowPosition)
